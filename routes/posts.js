26d5e540eb3194f2876074c79ef12f91502255bb
mojavemadman <mojavemadmangit@gmail.com>
2025-10-03 21:43:55 -0700

    completed routes with temp mem; add validation

--- routes/posts.js
+++ routes/posts.js
@@ -2,9 +2,32 @@ import { Router } from "express";
 
 const router = Router();
 
+let posts = [{
+            id : 4,
+            title: "The laxidasical learner",
+            content: "Here's how I mastered learning: have fun!",
+            category: "Education",
+            tags: ["learning", "lifestyle"],
+            createdAt: new Date().toISOString(),
+            updatedAt: new Date().toISOString(),
+        }];
+let nextId = 1;
+
 router.post("/", async (req, res) => {
     try {
-        res.send("Create post route");
+        const { title, content, category, tags } =  req.body;
+        const newPost = {
+            id : nextId,
+            title,
+            content,
+            category,
+            tags,
+            createdAt: new Date().toISOString(),
+            updatedAt: new Date().toISOString(),
+        }
+        posts.push(newPost);
+        nextId++;
+        res.status(201).send(newPost);
     } catch (error) {
         console.error("Error creating post:", error)
     }
@@ -12,7 +35,7 @@ router.post("/", async (req, res) => {
 
 router.get("/", async (req, res) => {
     try {
-    res.send("Get all  route");
+    res.status(200).send(posts);
     } catch (error) {
         console.error("Error getting :", error);
     }
@@ -20,8 +43,13 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
     try {
-        const postId = req.params.id;
-        res.send(`Get post ${postId} route`);
+        const postId = parseInt(req.params.id);
+        const post = posts.find(post => post.id === postId);
+
+        if (!post) {
+            return res.status(404).json({ error: "Post not found" });
+        }
+        res.status(200).send(post);
     } catch (error) {
         console.error("Error getting post:", error);
     }
@@ -29,8 +57,25 @@ router.get("/:id", async (req, res) => {
 
 router.put("/:id", async (req, res) => {
     try {
-        const postId = req.params.id;
-        res.send(`Update post ${postId} route`);
+        const postId = parseInt(req.params.id);
+        //Don't take id or createdAt to protect data
+        const { title, content, category, tags } = req.body;
+        const post = posts.find(post => post.id === postId);
+        
+        if (!post) {
+            return res.status(404).json({ error: "Post not found" });
+        }
+        //Filter for fields we allow to update & passed in body
+        const allowedUpdates = { title, content, categories, tags};
+        Object.keys(allowedUpdates).forEach(key => allowedUpdates[key] === undefined && delete allowedUpdates[key]);
+        //Double spread syntax allows overwriting original post (PUTception!)
+        const updatedPost = {
+            ...post,
+            ...allowedUpdates,
+            updatedAt: new Date().toISOString()
+        }
+        posts[posts.indexOf(post)] = updatedPost;
+        res.status(200).send(updatedPost);
     } catch (error) {
         console.error("Error updating post:", error);
     }
@@ -38,8 +83,17 @@ router.put("/:id", async (req, res) => {
 
 router.delete("/:id", async (req, res) => {
     try {
-        const postId = req.params.id;
-        res.send(`Delete post ${postId} route`);
+        const postId = parseInt(req.params.id);
+        const post = posts.find(post => post.id === postId);
+
+        if (!post) {
+            return res.status(404).json({ error: "Post not found"});
+        }
+
+        const index = posts.indexOf(post);
+        posts.splice(index, 1);
+
+        res.status(204).send();
     } catch (error) {
         console.error("Error deleting post:", error);
     }
---
+++ services/validate.js
@@ -0,0 +1,62 @@
+export default class Validate {
+    static title(title) {
+        if (!title) {
+            throw new Error("A title is required");
+        }
+
+        if (typeof title !== "string") {
+            throw new Error("Invalid title data type");
+        }
+
+        if (title.length > 50) {
+            throw new Error("Title is too long (max length 50 chars)");
+        }
+
+    }
+
+    static content(content) {
+        if (!content) {
+            throw new Error("The post has no content");
+        }
+
+        if (typeof content !== "string") {
+            throw new Error("Invalid content data type");
+        }
+
+        if (!/[a-zA-Z]/.test(content)) {
+            throw new Error("Please enter letters in the post")
+        }
+
+        if (content.length < 50) {
+            throw new Error("The post is too short (min length 50 chars)");
+        }
+
+        if (content.length > 500) {
+            throw new Error("The post is too long (max length 500 chars)");
+        }
+    }
+
+    static category(category) {
+        if (!category) {
+            throw new Error("The post has no category");
+        }
+
+        if (typeof category !== "string") {
+            throw new Error("Invalid category data type")
+        }
+
+        if (category.length > 15) {
+            throw new Error("Category is too long (max length 15 chars)");
+        }
+    }
+
+    static tags(tags) {
+        if (!Array.isArray(tags)) {
+            throw new Error("Invalid tags data type")
+        }
+
+        if (!tags.every(tag => typeof tag === "string")) {
+            throw new Error("One or more tag is an invalid data type")
+        }
+    }
+}
